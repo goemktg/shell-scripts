04c5d6b8e7567594733fd9ea0a5edec15549f18e
goemktg <samktg52@gmail.com>
2024-02-04 04:00:38 +0900

    Initial commit

---
+++ launcher.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if ! [[ "$1" =~ ^(start|stop|update)$ ]]; then
+    echo "Usage: launcher COMMAND"
+    echo "Commands:"
+    echo "    start:       Start/initialize a stack"
+    echo "    stop:        Stop a running stack"
+    echo "    update:      Update stack"
+    exit 1
+fi
+
+function start_stack() {
+    echo "Starting seat-docker application..."
+    docker compose -f docker-compose.yml -f docker-compose.mariadb.yml -f docker-compose.traefik.yml -f docker-compose.overload.yml up -d
+
+    echo "Deploying additional apache2 config..."
+    docker compose cp apache-realip-config/use-x-forwarded-for-as-ip.conf front:/etc/apache2/conf-enabled
+
+    # check apache2 running
+    while [ -z "$(ps aux | grep apache2 | grep -v grep)" ]; do
+        echo "apache2 service might not be ready yet. Sleeping..."
+        sleep 1 # wait 1 sec
+    done
+
+    echo "Start enabling module remoteip..."
+    docker compose exec --user root front a2enmod remoteip
+
+    echo "Start reloading apache2 service..."
+    docker compose exec --user root front service apache2 reload
+}
+
+case $1 in
+start)
+    echo "starting seat stack..."
+    start_stack
+    ;;
+stop)
+    echo "stopping seat stack..."
+    docker compose -f docker-compose.yml -f docker-compose.mariadb.yml -f docker-compose.traefik.yml -f docker-compose.overload.yml down
+    ;;
+update)
+    echo "updating seat stack..."
+
+    echo "pulling latest dockerhub images..."
+    docker compose -f docker-compose.yml -f docker-compose.mariadb.yml -f docker-compose.traefik.yml pull
+
+    echo "taking stack down..."
+    docker compose -f docker-compose.yml -f docker-compose.mariadb.yml -f docker-compose.traefik.yml down
+
+    echo "bringing stack back online..."
+    start_stack
+
+    echo "cleaning any dangling images..."
+    docker image prune -f
+    ;;
+esac
